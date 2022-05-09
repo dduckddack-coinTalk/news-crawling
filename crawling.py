c0e5c77008b0a158cbcd998b2dd187fcb8175343
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-05-09 20:25:15 +0900

    feat: whlile문으로 페이지 넘겨가면서 저장

--- crawling.py
+++ crawling.py
@@ -3,31 +3,37 @@ from bs4 import BeautifulSoup
 
 print('뉴스기사 스크래핑 시작')
 
-
-req = requests.get('https://www.coindeskkorea.com/news/articleList.html?page=2&total=11166&box_idxno=&view_type=sm')
-req.encoding = None
-html = req.content
-soup = BeautifulSoup(html, 'html.parser')
-datas = soup.select(
-    'div.list-block'
-)
-
+index = 1
 list = []
 
-for data in datas:
-    title = data.select_one("div.text-block > div.list-titles").text.strip()  # 제목
-    editorCreatedAt = data.select_one("div.text-block > div.list-dated").text.strip()  # 작성자 + 작성일
-    createdAt = editorCreatedAt[editorCreatedAt.find(' ') + 1:]  # 작성일
-    editor = editorCreatedAt[:editorCreatedAt.find(' ')]  # 작성자
-    summary = data.select_one("div.text-block > p.list-summary").text.strip()  # 본문
-    url = data.select_one("div.text-block > p.list-summary >a ")["href"]  # link
+while index < 3:
+    url = "https://www.coindeskkorea.com/news/articleList.html?page="+str(index)+"&total=11166&box_idxno=&view_type=sm"
+    req = requests.get(url)
+    req.encoding = None
+    html = req.content
+    soup = BeautifulSoup(html, 'html.parser')
+    datas = soup.select(
+        'div.list-block'
+    )
 
-    list.append({
-        "title": title,
-        "editor": editor,
-        "createdAt": createdAt,
-        "summary": summary,
-        "url": url
-    })
+
+
+    for data in datas:
+        title = data.select_one("div.text-block > div.list-titles").text.strip()  # 제목
+        editorCreatedAt = data.select_one("div.text-block > div.list-dated").text.strip()  # 작성자 + 작성일
+        createdAt = editorCreatedAt[editorCreatedAt.find(' ') + 1:]  # 작성일
+        editor = editorCreatedAt[:editorCreatedAt.find(' ')]  # 작성자
+        summary = data.select_one("div.text-block > p.list-summary").text.strip()  # 본문
+        url = data.select_one("div.text-block > p.list-summary >a ")["href"]  # link
+
+        list.append({
+            "title": title,
+            "editor": editor,
+            "createdAt": createdAt,
+            "summary": summary,
+            "url": url
+        })
+
+    index += 1
 
 print('뉴스기사 스크래핑 끝')
