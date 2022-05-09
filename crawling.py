55b9c4bd20cf6a146770b81c7ff7e6d558011f70
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-05-09 20:38:07 +0900

    feat : imgUrl 추가

--- crawling.py
+++ crawling.py
@@ -5,8 +5,9 @@ print('뉴스기사 스크래핑 시작')
 
 index = 1
 list = []
+newsDomain = "https://www.coindeskkorea.com"
 
-while index < 3:
+while index < 2:
     url = "https://www.coindeskkorea.com/news/articleList.html?page="+str(index)+"&total=11166&box_idxno=&view_type=sm"
     req = requests.get(url)
     req.encoding = None
@@ -20,11 +21,16 @@ while index < 3:
 
     for data in datas:
         title = data.select_one("div.text-block > div.list-titles").text.strip()  # 제목
+
+        style = data.select_one("div.list-block > div.list-image")["style"]
+        imgUrl = newsDomain + "/news" + style[style.find("./") +1 :-1]
+
         editorCreatedAt = data.select_one("div.text-block > div.list-dated").text.strip()  # 작성자 + 작성일
         createdAt = editorCreatedAt[editorCreatedAt.find(' ') + 1:]  # 작성일
         editor = editorCreatedAt[:editorCreatedAt.find(' ')]  # 작성자
+
         summary = data.select_one("div.text-block > p.list-summary").text.strip()  # 본문
-        url = data.select_one("div.text-block > p.list-summary >a ")["href"]  # link
+        newUrl = newsDomain + data.select_one("div.text-block > p.list-summary >a ")["href"]  # link
 
         list.append({
             "title": title,
