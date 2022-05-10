af0609681e095f850fbb7f51c8606c70843a86f2
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-05-10 18:45:02 +0900

    fix

--- githubIssue.py
+++ githubIssue.py
@@ -10,7 +10,6 @@ def make_issue(news):
     REPO_NAME = 'https://github.com/dduckddack-coinTalk/news-crawling.git'
 
     g = Github(ISSUE_TOKEN)
-    repo = Github(ISSUE_TOKEN).get_user().get_repo(REPO_NAME)
     repo = g.get_organization("dduckddack-coinTalk").get_repo("news-crawling")
 
     repo.create_issue(title=title, body=body)
